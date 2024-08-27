0ebdbc7ec1911a67f9a01b98d3de6fbf430c6aa4
bielgennaro <zirondigabriel@gmail.com>
2024-08-27 00:34:34 -0300

    Simplificação da lógica de movimentação e captura de exceções
    Removida a lógica de animação de caminhada e variáveis associadas
    (_animator, _isWalking, _anim). A movimentação agora é feita
    diretamente com transform.Translate usando os eixos "Horizontal"
    e "Vertical". Adicionada estrutura try-catch no método Update
    para capturar e logar exceções com Debug.LogError, melhorando
    a robustez do código.

--- Assets/Scripts/Walking/Walk.cs
+++ Assets/Scripts/Walking/Walk.cs
@@ -5,42 +5,26 @@ namespace Assets.TutorialInfo.Scripts
 {
     public class Walk : MonoBehaviour
     {
-        private Animator _animator;
         private bool _isWalking;
-        private  Animator _anim;
 
         // Use this for initialization
         void Start()
         {
-            _animator = GetComponent<Animator>();
         }
 
         // Update is called once per frame
         void Update()
         {
-            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+            try
+            { 
+                var translation = Input.GetAxis("Vertical") * 5.0f * Time.deltaTime;
+                var strafe = Input.GetAxis("Horizontal") * 5.0f * Time.deltaTime;
 
-
-            transform.Rotate(0, x, 0);
-
-            if (Input.GetKey(KeyCode.W))
-            {
-                if (!_isWalking)
-                {
-                    _isWalking = true;
-                    _anim.Play("Walk");
-                }
-
-                var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-                transform.Rotate(0, 0, z); // Só se movimenta quando "W" for apertado
+                transform.Translate(strafe, 0, translation);
             }
-            else
+            catch (System.Exception ex)
             {
-                if( _isWalking )
-                {
-                    _anim.Play("Grounded", -1);
-                }
-                _isWalking = false;
+                Debug.LogError(ex.Message);
             }
         }
     }
