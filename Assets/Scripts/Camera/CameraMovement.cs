27987448b8afee5c45b65f278c08bf5b5bdcc1fe
bielgennaro <zirondigabriel@gmail.com>
2024-08-30 00:32:47 -0300

    Renomeia Main Camera e adiciona script de movimento
    - Renomeado GameObject "Main Camera" para "Camera".
    - Atualizados `fileID`s dos componentes associados ao GameObject.
    - Alterada posição local do Transform para `{x: 0, y: 0.459, z: 0}`.
    - Alterado pai do Transform para `{fileID: 1738326451}`.
    - Removido e adicionado novamente `AudioListener` com novo `fileID`.
    - Removido e adicionado novamente `Camera` com novo `fileID` e `field of view` alterado para `80`.
    - Atualizado `MonoBehaviour` com novos valores para `antialiasing`, `dithering`, `msaaMode`, e `data1`.
    - Adicionado novo GameObject "Capsule" com vários componentes.
    - Adicionado script `CameraMovement.cs` para controle de movimentação da câmera.
    - Adicionado arquivo `CameraMovement.cs.meta`.
    - Atualizado `ProjectSettings.asset` para remover configuração automática para `WindowsStandaloneSupport`.
    - Adicionado novo arquivo `Camera.meta`.

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CameraMovement : MonoBehaviour
+    {
+        public float mouseSensitivity = 100.0f; // Futuramente será ajustável pelo player óbvio
+        public Transform playerBody;
+        private float xRotation = 0.0f;
+        private float yRotation = 0.0f;
+
+        // Use this for initialization
+        void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked; // Locka o cursor no centrda tela
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Pega o movimento do mouse (não faço idéia o motivo de multiplicar por Time.deltaTime) :)
+            var mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; 
+            var mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime; 
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, 90.0f, -90.0f); // Limita o movimento da câmera
+
+            yRotation -= mouseX;
+            yRotation = Mathf.Clamp(yRotation, 90.0f, -90.0f);
+
+            // Sepa que a mágica acontece aqui
+            transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0.0f);
+            playerBody.Rotate(mouseX * Vector3.up);
+        }
+    }
+}
